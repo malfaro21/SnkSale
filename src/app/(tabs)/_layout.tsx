b8451a4916c833c3bf484a62625b8aa58ffd1c08
SergioGranados25 <103449004+SergioGranados25@users.noreply.github.com>
2024-02-23 10:49:09 -0500

    Update _layout.tsx

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,9 +1,9 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Link, Tabs } from 'expo-router';
-import { Pressable } from 'react-native';
+import FontAwesome from '@expo/vector-icons/FontAwesome'; // imports FontAwesome which is an icon library
+import { Link, Tabs } from 'expo-router'; // imports the Link and Tabs components from the expo library
+import { Pressable } from 'react-native'; // imports the Pressable objects from the react library
 
-import Colors from '@constants/Colors';
-import { useColorScheme } from '@components/useColorScheme';
+import Colors from '@constants/Colors'; // imports the defined colors from the Colors file in the constants folder
+import { useColorScheme } from '@components/useColorScheme'; // 
 import { useClientOnlyValue } from '@components/useClientOnlyValue';
 
 // You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
