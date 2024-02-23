9f1ffcb1dd1f1efb360ed5934bf2e646faf8ee55
Cesar Bautista <113205450+CesarB2025@users.noreply.github.com>
2024-02-23 10:36:58 -0500

    added descriptions

--- src/constants/Colors.ts
+++ src/constants/Colors.ts
@@ -1,5 +1,5 @@
-const tintColorLight = '#2f95dc';
-const tintColorDark = '#fff';
+const tintColorLight = '#2f95dc'; //It's a hexadecimal color value, and corresponds to a specific shade of blue
+const tintColorDark = '#fff'; //Its a hexadecimal color value, and it defines a color for dark mode or a darker theme.
 
 export default {
   light: {
