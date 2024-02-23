aeb284b8c18177ec942c69d51aa44c210e26ee22
jdmunoz21 <103907161+jdmunoz21@users.noreply.github.com>
2024-02-23 14:03:17 -0500

    two comments

--- src/app/(tabs)/two.tsx
+++ src/app/(tabs)/two.tsx
@@ -1,31 +1,32 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '@components/EditScreenInfo';
-import { Text, View } from '@components/Themed';
+import { StyleSheet } from 'react-native'; // import to create stylesheets
+import EditScreenInfo from '@components/EditScreenInfo'; // import to access editscreeninfo components
+import { Text, View } from '@components/Themed'; // import to add views and text
 
 export default function TabTwoScreen() {
   return (
+    // sets tab title and seperator with proper styles
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
+      <Text style={styles.title}>Tab Two</Text> 
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/two.tsx" />
     </View>
   );
 }
 
+// styles for everything on page
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+  container: { // style for page container
+    flex: 1, // controls how component fills available space
+    alignItems: 'center', // aligns item to center
+    justifyContent: 'center', // describes how to align children
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
+  title: { // style for tab title
+    fontSize: 20, // sets font size to 20
+    fontWeight: 'bold', // makes font bold
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  separator: { // style for separator
+    marginVertical: 30, // sets top and bottom margins
+    height: 1, // determines height on screen
+    width: '80%', // determines width on screen
   },
 });
