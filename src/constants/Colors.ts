a45d43d4e7d1f736cdccb6acd9d6cc0678e60b17
Cesar Bautista <113205450+CesarB2025@users.noreply.github.com>
2024-02-23 10:52:00 -0500

    final changes

--- src/constants/Colors.ts
+++ src/constants/Colors.ts
@@ -1,19 +1,19 @@
 const tintColorLight = '#2f95dc'; //It's a hexadecimal color value, and corresponds to a specific shade of blue
 const tintColorDark = '#fff'; //Its a hexadecimal color value, and it defines a color for dark mode or a darker theme.
 
-export default {
-  light: {
-    text: '#000',
-    background: '#fff',
-    tint: tintColorLight,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorLight,
+export default { //It defines an object that represents a theme.
+  light: { //This property is assigned an object as its value, and the opening curly brace { indicates the start of this object.
+    text: '#000', //It defines the color for text elements in the light theme. It represents the color black
+    background: '#fff', //It defines the background color for the light theme. It represents the color white.
+    tint: tintColorLight, //It represents a tint color used for accenting certain elements
+    tabIconDefault: '#ccc',//It defines the default color for tab icons in the light theme. It represents a light gray color.
+    tabIconSelected: tintColorLight, //It defines the color for selected tab icons in the light theme
   },
-  dark: {
-    text: '#fff',
-    background: '#000',
-    tint: tintColorDark,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorDark,
+  dark: { //This property is assigned an object as its value, and the opening curly brace { indicates the start of this object.
+    text: '#fff', //It defines the color for text elements in the dark theme. It represents the color white
+    background: '#000', //It defines the background color for the dark theme. It represents the color black.
+    tint: tintColorDark, //It represents a tint color used for accenting certain elements
+    tabIconDefault: '#ccc', //It defines the default color for tab icons in the dark theme. It represents a light gray color.
+    tabIconSelected: tintColorDark, //Its defines the color for selected tab icons in the dark theme
   },
 };
