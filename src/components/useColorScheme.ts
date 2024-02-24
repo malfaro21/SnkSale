7174b60ed40e8d8f0fcec7823cc00f5e7dc1cc71
Cesar Bautista <113205450+CesarB2025@users.noreply.github.com>
2024-02-24 17:18:15 -0500

    portion completed

--- src/components/useColorScheme.ts
+++ src/components/useColorScheme.ts
@@ -1,4 +1,2 @@
 export { useColorScheme } from 'react-native';
-//The "export" keyword indicates that the following function (useColorScheme) will be exported from the module
-//The name "useColorScheme" is the export statement
-//from 'react-native': This part specifies the module from which the exported bindings are sourced
+//This line of code exports a function named useColorScheme from the 'react-native' module.
