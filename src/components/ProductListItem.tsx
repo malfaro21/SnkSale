59871aa0cb9fb1d8b29c6dbdf70cb6144d3acdd8
Cesar Bautista <113205450+CesarB2025@users.noreply.github.com>
2024-02-24 18:51:13 -0500

    portion completed

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,27 +1,27 @@
-import { StyleSheet, Text, View, Image, Pressable } from 'react-native';
-import Colors from '@constants/Colors';
-import { Product } from '../types';
-import { Link } from 'expo-router';
+import { StyleSheet, Text, View, Image, Pressable } from 'react-native';//This line of code imports several components and utilities from the 'react-native' library.
+import Colors from '@constants/Colors';//This line of code imports the Colors object from a module located at the path '@constants/Colors'.
+import { Product } from '../types';//This line of code imports the Product type from a module located at the path '../types'.
+import { Link } from 'expo-router';//The line of code imports the Link component from a module named 'expo-router'.
 
 export const defaultShoeImage =
-'https://shorturl.at/fvEZ3';
+'https://shorturl.at/fvEZ3';//This line of code exports a constant named defaultShoeImage with a value of 'https://shorturl.at/fvEZ3'
 
 
 type ProductListItemProps = {
-    product: Product
+    product: Product//This line of code defines a TypeScript type named ProductListItemProps.
 }
 
-const ProductListItem =({ product }: ProductListItemProps) => {
+const ProductListItem =({ product }: ProductListItemProps) => {//This code defines a React functional component named ProductListItem. It takes a single parameter, product
   return(
-    <Link href={`/(tabs)/sneakers/${product.id}`} asChild>
+    <Link href={`/(tabs)/sneakers/${product.id}`} asChild> 
     <Pressable style={styles.container}>
       <Image 
-      source={{uri: product.image || defaultShoeImage}} style={styles.image} 
+      source={{uri: product.image || defaultShoeImage}} style={styles.image} //displaying an image of a product
       resizeMode='cover'
-      
      />
 
-      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.title}>{product.name}</Text> 
+
       <Text style={styles.price}>${product.price}</Text>
 
     </Pressable>
@@ -29,16 +29,16 @@ const ProductListItem =({ product }: ProductListItemProps) => {
   );
 };
 
-export default ProductListItem;
+export default ProductListItem;//This line of code exports the ProductListItem component as the default export from the module.
 
 const styles = StyleSheet.create({
-  container: {
+  container: {// this code adjusts the backgroundColor, padding, borderRadius, and flex all inside the component called container
     backgroundColor: 'white',
     padding: 10,
     borderRadius: 20,
     flex: 1,
   },
-  image:{
+  image:{// adjust the image width to 100% and aspectRation by 2/1
     width: '100%',
     aspectRatio: 2/1,
   },
