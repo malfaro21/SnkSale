d6bb682cb4d9cc8cd00aee043ef95d5aa47b0272
Cesar Bautista <113205450+CesarB2025@users.noreply.github.com>
2024-02-24 17:24:19 -0500

    portion completed

--- src/components/StyledText.tsx
+++ src/components/StyledText.tsx
@@ -1,5 +1,5 @@
-import { Text, TextProps } from './Themed';
+import { Text, TextProps } from './Themed';//This line of code imports the Text component and the TextProps type from the './Themed' module.
 
-export function MonoText(props: TextProps) {
-  return <Text {...props} style={[props.style, { fontFamily: 'SpaceMono' }]} />;
+export function MonoText(props: TextProps) {//This line of code defines a function named MonoText that takes an object called props as its input parameter.
+  return <Text {...props} style={[props.style, { fontFamily: 'SpaceMono' }]} />;//This line of code returns a JSX element representing a Text component
 }
