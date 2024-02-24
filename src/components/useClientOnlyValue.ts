93e3e28244e861b58d69a4d12339580023752b1b
Cesar Bautista <113205450+CesarB2025@users.noreply.github.com>
2024-02-24 17:19:22 -0500

    portion completed

--- src/components/useClientOnlyValue.ts
+++ src/components/useClientOnlyValue.ts
@@ -1,12 +1,6 @@
 // This function is web-only as native doesn't currently support server (or build-time) rendering.
 export function useClientOnlyValue<S, C>(server: S, client: C): S | C { 
-  //The "export" keyword indicates that the following function (useClientOnlyValue) will be exported from the module and can be imported and used in other modules.
-  //The "function" keyword denotes the start of a function declaration.
-  //useClientOnlyValue is the name of the function being defined
-  //<S, C>: Are generic type parameters for the function
-  //"server: S, client: C" is the parameter list of the function
-  //"S | C" indicates the return type of the function
+  //This line of code defines a function named useClientOnlyValue that takes two parameters: server of type S and client of type C. The function returns a value of type S or C.
   return client;
-  // The "return" keyword is used within a function to specify the value that the function will produce when it's called.
-  //The "client" keyword is the value that the function is returning.
+  // This line of code is part of a function and it specifies that the function should return the value of the client parameter.
 }
