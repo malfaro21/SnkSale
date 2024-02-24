c3e37a4234751d94634e2f61929c678b1e7a097f
Cesar Bautista <113205450+CesarB2025@users.noreply.github.com>
2024-02-24 17:22:40 -0500

    portion completed

--- src/components/useClientOnlyValue.web.ts
+++ src/components/useClientOnlyValue.web.ts
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from 'react';//This line of code imports the React module from the 'react' library.
 
 // `useEffect` is not invoked during server rendering, meaning
 // we can use this to determine if we're on the server or not.
-export function useClientOnlyValue<S, C>(server: S, client: C): S | C {
-  const [value, setValue] = React.useState<S | C>(server);
-  React.useEffect(() => {
-    setValue(client);
+export function useClientOnlyValue<S, C>(server: S, client: C): S | C {//This line of code declares a function named useClientOnlyValue that takes two generic parameters S and C
+  const [value, setValue] = React.useState<S | C>(server);//This line of code initializes a state variable value using the useState hook from React
+  React.useEffect(() => {//This line of code is the beginning of a useEffect hook in a React component
+    setValue(client);//This line of code sets the state variable value to the client parameter passed to the component.
   }, [client]);
 
-  return value;
+  return value;//This line of code returns the current value of the state variable value
 }
