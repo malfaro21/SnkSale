5448027bf82a9b248a457a83118d40d56a64b8ef
Cesar Bautista <113205450+CesarB2025@users.noreply.github.com>
2024-02-24 17:17:32 -0500

    portion completed

--- src/components/useColorScheme.web.ts
+++ src/components/useColorScheme.web.ts
@@ -3,8 +3,6 @@
 // and the first render on the client. Typically, web developers will use CSS media queries
 // to render different styles on the client and server, these aren't directly supported in React Native
 // but can be achieved using a styling library like Nativewind.
-export function useColorScheme() {// The "export" keyword indicates that the following function (useColorScheme) will be exported from the module
-                                  // The "function" keyword denotes the start of a function declaration
-                                  // The pair of parentheses in "useColorScheme()" denotes the parameter list of the function. However, in this case, the function doesn't accept any parameters.
-  return 'light'; // The "return" keyword specifies the value that the function will produce when invoked, in this case its the function "light"
+export function useColorScheme() {// This line of code is the beginning of a function declaration named useColorScheme.
+  return 'light'; // The "return" keyword specifies the value, in this case its the function "light"
 }
