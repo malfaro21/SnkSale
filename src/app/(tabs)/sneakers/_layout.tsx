c17e84cb7b11bf8c75c5179ba8ac0dfe51928ae0
malfaro21 <112723730+malfaro21@users.noreply.github.com>
2024-02-25 12:08:42 -0500

    Comments to _layout.tsx

--- src/app/(tabs)/sneakers/_layout.tsx
+++ src/app/(tabs)/sneakers/_layout.tsx
@@ -1,7 +1,7 @@
-import { Stack} from "expo-router";
+import { Stack} from "expo-router"; //imports Stack component, Stack can be used to manage navigation in between screens
 
-export default function SneakersStack (){
-    return <Stack>
-        <Stack.Screen name="index" options={{title: 'Sneakers'}}/>
+export default function SneakersStack (){ //exports SnaeakerStack as the default function to be used in other modules
+    return <Stack> //creates a stack
+        <Stack.Screen name="index" options={{title: 'Sneakers'}}/> //defines a screen index with the title as Sneakers
     </Stack>;
 }
