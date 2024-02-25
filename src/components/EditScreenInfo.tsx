83777ab2c251de13a69c949470cf9994fc412827
Cesar Bautista <113205450+CesarB2025@users.noreply.github.com>
2024-02-24 21:36:47 -0500

    portion completed

--- src/components/EditScreenInfo.tsx
+++ src/components/EditScreenInfo.tsx
@@ -1,33 +1,33 @@
-import React from 'react';
-import { StyleSheet } from 'react-native';
+import React from 'react';//This line of code imports the React library/module into the current file
+import { StyleSheet } from 'react-native';//This line of code imports the StyleSheet utility from the 'react-native' package
 
-import { ExternalLink } from './ExternalLink';
-import { MonoText } from './StyledText';
-import { Text, View } from './Themed';
+import { ExternalLink } from './ExternalLink';//This line of code imports the ExternalLink component from a file named 'ExternalLink.js'
+import { MonoText } from './StyledText';//This line of code imports the MonoText component from a file named 'StyledText.js
+import { Text, View } from './Themed';//This line of code imports the Text and View components from a file named 'Themed.js'
 
-import Colors from '@constants/Colors';
+import Colors from '@constants/Colors';//This line of code imports a module named Colors
 
-export default function EditScreenInfo({ path }: { path: string }) {
+export default function EditScreenInfo({ path }: { path: string }) {//This code exports a function named EditScreenInfo
   return (
     <View>
       <View style={styles.getStartedContainer}>
         <Text
-          style={styles.getStartedText}
-          lightColor="rgba(0,0,0,0.8)"
+          style={styles.getStartedText}//the appearance of the component will be determined by the styles defined in the getStartedText
+          lightColor="rgba(0,0,0,0.8)"//The lightColor attribute is setting the color of the text to a semi-transparent black 
           darkColor="rgba(255,255,255,0.8)">
           Open up the code for this screen:
         </Text>
 
         <View
-          style={[styles.codeHighlightContainer, styles.homeScreenFilename]}
-          darkColor="rgba(255,255,255,0.05)"
+          style={[styles.codeHighlightContainer, styles.homeScreenFilename]}//attribute applies two styles to the component
+          darkColor="rgba(255,255,255,0.05)"// attribute sets the color of the text to a very light gray
           lightColor="rgba(0,0,0,0.05)">
           <MonoText>{path}</MonoText>
         </View>
 
         <Text
-          style={styles.getStartedText}
-          lightColor="rgba(0,0,0,0.8)"
+          style={styles.getStartedText}//ttribute applies the style named getStartedText from the styles object to the component
+          lightColor="rgba(0,0,0,0.8)"//attribute sets the color of the text to a semi-transparent black when the current theme is light.
           darkColor="rgba(255,255,255,0.8)">
           Change any of the text, save the file, and your app will automatically update.
         </Text>
@@ -35,7 +35,7 @@ export default function EditScreenInfo({ path }: { path: string }) {
 
       <View style={styles.helpContainer}>
         <ExternalLink
-          style={styles.helpLink}
+          style={styles.helpLink}//attribute applies the style named helpLink from the styles object to the component
           href="https://docs.expo.io/get-started/create-a-new-app/#opening-the-app-on-your-phonetablet">
           <Text style={styles.helpLinkText} lightColor={Colors.light.tint}>
             Tap here if your app doesn't automatically update after making changes
@@ -47,31 +47,31 @@ export default function EditScreenInfo({ path }: { path: string }) {
 }
 
 const styles = StyleSheet.create({
-  getStartedContainer: {
+  getStartedContainer: {//alignItems to the center, and marginHorizontal is set to 50 on the component getStartedContainer
     alignItems: 'center',
     marginHorizontal: 50,
   },
-  homeScreenFilename: {
+  homeScreenFilename: {// marginVertical is set at 7 in homeScreenFilename
     marginVertical: 7,
   },
-  codeHighlightContainer: {
+  codeHighlightContainer: {//borderRadius is set at 3 and paddingHorizontal is set at 4 on component codeHighlightContainer
     borderRadius: 3,
     paddingHorizontal: 4,
   },
-  getStartedText: {
+  getStartedText: {// fontSize is set at 17, lineHeight is set at 24, and textAlign is set to center on component getStartedText
     fontSize: 17,
     lineHeight: 24,
     textAlign: 'center',
   },
-  helpContainer: {
+  helpContainer: {// marginTop is set to 15, marginHorizontal is set to 20, and alignItems is set to center on component helpContainer
     marginTop: 15,
     marginHorizontal: 20,
     alignItems: 'center',
   },
-  helpLink: {
+  helpLink: {// paddingVertical is set to 15 on component 15
     paddingVertical: 15,
   },
-  helpLinkText: {
+  helpLinkText: {// textAlign is set to center on the component helpLinkText
     textAlign: 'center',
   },
 });
