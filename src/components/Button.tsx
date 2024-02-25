1a1a9851b02e7da705e35c09bd3379df4f434ecf
Cesar Bautista <113205450+CesarB2025@users.noreply.github.com>
2024-02-24 22:03:56 -0500

    portion completed

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,30 +1,30 @@
-import { Pressable, StyleSheet, Text, View } from 'react-native';
-import Colors from '../constants/Colors';
-import { forwardRef } from 'react';
+import { Pressable, StyleSheet, Text, View } from 'react-native';//This line of code brings in important tools from the 'react-native'
+import Colors from '../constants/Colors';//This line of code imports a module named Colors
+import { forwardRef } from 'react';//This line of code imports a function called forwardRef from the react package.
 
 type ButtonProps = {
   text: string;
-} & React.ComponentPropsWithoutRef<typeof Pressable>;
+} & React.ComponentPropsWithoutRef<typeof Pressable>;//This code defines a type named "ButtonProps". It describes the props that can be passed to a button component
 
 const Button = forwardRef<View | null, ButtonProps>(
-  ({ text, ...pressableProps }, ref) => {
+  ({ text, ...pressableProps }, ref) => {//This code defines a functional component named "Button" using the forwardRef function
     return (
       <Pressable ref={ref} {...pressableProps} style={styles.container}>
         <Text style={styles.text}>{text}</Text>
-      </Pressable>
+      </Pressable>//This part of the code defines the JSX structure for the Button component
     );
   }
 );
 
 const styles = StyleSheet.create({
-  container: {
+  container: {//set the backgroundColor to whatever Colors.light.tint is, padding is st to padding to 15, alignItems is set to center, borderRadius set to 100 ,and marginVertical is set to 10 on component container
     backgroundColor: Colors.light.tint,
     padding: 15,
     alignItems: 'center',
     borderRadius: 100,
     marginVertical: 10,
   },
-  text: {
+  text: { // fontSize is set to 16, fontWeight to 600 ,and color is set to white on component text
     fontSize: 16,
     fontWeight: '600',
     color: 'white',
