8fb104b61398d1622c614aec017a1e86682eec72
SergioGranados25 <103449004+SergioGranados25@users.noreply.github.com>
2024-02-26 13:36:57 -0500

    Update +not-found.tsx

--- src/app/+not-found.tsx
+++ src/app/+not-found.tsx
@@ -1,16 +1,16 @@
-import { Link, Stack } from 'expo-router';
-import { StyleSheet } from 'react-native';
+import { Link, Stack } from 'expo-router';  // Imports Link and Stack objects from the expo-router library
+import { StyleSheet } from 'react-native';  // Imports StyleSheet from the react-native Library
 
-import { Text, View } from '@components/Themed';
+import { Text, View } from '@components/Themed';  // imports Text and View that we defined in the Theme file in the components folder
 
-export default function NotFoundScreen() {
+export default function NotFoundScreen() { {/* exports the NotFoundScreen, which is a failsafe for where the user accesses a screen they are not supposed to. This directs them back to the home screen.*/}
   return (
     <>
-      <Stack.Screen options={{ title: 'Oops!' }} />
-      <View style={styles.container}>
-        <Text style={styles.title}>This screen doesn't exist.</Text>
+      <Stack.Screen options={{ title: 'Oops!' }} />  {/* Creates a title that says ' Oops! ' */}
+      <View style={styles.container}>      {/* Creates a view with the style set to container*/}
+        <Text style={styles.title}>This screen doesn't exist.</Text>  {/* creates a text object with the title styling*/}
 
-        <Link href="/" style={styles.link}>
+        <Link href="/" style={styles.link}>  {/* creates a link that directs the user to the homepage */}
           <Text style={styles.linkText}>Go to home screen!</Text>
         </Link>
       </View>
@@ -18,7 +18,7 @@ export default function NotFoundScreen() {
   );
 }
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create({   {/* Creates s stylesheet for the not-found function*/}
   container: {
     flex: 1,
     alignItems: 'center',
