435f5aa8c0851cbeb7a0717a32d684b546677ab1
SergioGranados25 <103449004+SergioGranados25@users.noreply.github.com>
2024-02-26 14:19:27 -0500

    Update _layout.tsx

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,11 +1,11 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
+import FontAwesome from '@expo/vector-icons/FontAwesome';  // imports the FontAwesome library from the expo library
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';  // imports the DarkTheme, DefaultTheme, and ThemeProvider from the react library
+import { useFonts } from 'expo-font'; // imports the useFont object from the expo-font library
+import { Stack } from 'expo-router'; // imports the Stack object from the expo-router library
+import * as SplashScreen from 'expo-splash-screen'; // imports everything from expo-splash-screen and names it SpashScreeen
+import { useEffect } from 'react'; // imports useEffect from the react library
 
-import { useColorScheme } from '@components/useColorScheme';
+import { useColorScheme } from '@components/useColorScheme'; // imports useColorScheme function we defiened in the components file
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -20,7 +20,7 @@ export const unstable_settings = {
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
-export default function RootLayout() {
+export default function RootLayout() {  {/* exports the RootLayout function*/}
   const [loaded, error] = useFonts({
     SpaceMono: require('../../assets/fonts/SpaceMono-Regular.ttf'),
     ...FontAwesome.font,
@@ -44,11 +44,11 @@ export default function RootLayout() {
   return <RootLayoutNav />;
 }
 
-function RootLayoutNav() {
+function RootLayoutNav() { {/* creates the function RootLayoutNav which sents the colorscheme to the useColorScheme function*/}
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>  {/* lets you create your own theme */}
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
