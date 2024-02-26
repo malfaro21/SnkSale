89ac17c9afbbdd2acebf503a944e92406f512150
SergioGranados25 <103449004+SergioGranados25@users.noreply.github.com>
2024-02-26 13:12:19 -0500

    Update modal.tsx

--- src/app/modal.tsx
+++ src/app/modal.tsx
@@ -1,15 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { Platform, StyleSheet } from 'react-native';
+import { StatusBar } from 'expo-status-bar'; //import 'expo-status-bar' from StatusBar
+import { Platform, StyleSheet } from 'react-native'; // import the react-native stylesheet and platform which Returns an object which contains all available common and specific constants related to the platform
 
-import EditScreenInfo from '@components/EditScreenInfo';
-import { Text, View } from '@components/Themed';
+import EditScreenInfo from '@components/EditScreenInfo'; // imports EditScreenInfo which we defined in the components folder
+import { Text, View } from '@components/Themed'; //imports the Text and View that we defined in the components folder
 
-export default function ModalScreen() {
+export default function ModalScreen() {     // exports this function which is made up of the following lines
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Modal</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/modal.tsx" />
+    <View style={styles.container}>     {/*creates a view with the style set to a container*/} 
+      <Text style={styles.title}>Modal</Text>  {/*creates a text line with the style set to title*/}
+      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />  {/* Creates another view with the style set to separator and gives it a light color and a dark color*/}
+      <EditScreenInfo path="app/modal.tsx" />     {/* sets the path of whatever screen uses this function to app/modal.tsk*/}
 
       {/* Use a light status bar on iOS to account for the black space above the modal */}
       <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
@@ -17,7 +17,7 @@ export default function ModalScreen() {
   );
 }
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create({           {/* creates a constant stylesheet with the below properties*/}
   container: {
     flex: 1,
     alignItems: 'center',
